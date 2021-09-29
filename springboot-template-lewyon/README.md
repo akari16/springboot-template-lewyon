a32ff3e2ae1d45f96d7299c28ae8f178663e1ba2
lewyon <akari16@163.com>
2021-09-29 17:16:28 +0800

    添加swagger注解和restful简单的api

--- springboot-template-lewyon/README.md
+++
@@ -1,4 +0,0 @@
-# springboot-template-lewyon
-
-- springboot基础项目模板
-- 技术栈:spring boot、mybatis
--- src/main/java/com/lewyon/template/TemplateApplication.java
+++ src/main/java/com/lewyon/template/TemplateApplication.java
@@ -1,13 +1,16 @@
 package com.lewyon.template;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableSwagger2Doc
 @SpringBootApplication
-public class DemoApplication {
+public class TemplateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(TemplateApplication.class, args);
     }
 
 }
